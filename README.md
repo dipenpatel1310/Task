d7317a21d7c5cbe1184d9def7014a45329caeae9
Dipen <dipenmotka@gmail.com>
2022-08-09 17:08:54 +0530

    Added Task 2

---
+++ README.md
@@ -0,0 +1,11 @@
+# Node JS Tasks
+> by: _Dipen Patel_
+
+### Task 1
+* Run the `app.js` 
+* Enter the number up to which you want to find the numbers of fibonacci
+
+### Task 2
+* Run the `app.js`
+* Enter the number up to which you want to find the numbers of fibonacci
+* results will be stored in `data.csv` with the required format.
--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+const csv = require('csv-parser');
+const fs = require('fs');
 
+var arr=[]
+
+    
+// *****************Fib func****************
 var fib = function(n) {
     if (n === 0) {
       return [0];
@@ -12,12 +18,52 @@ var fib = function(n) {
     }
   };
   
+//   console.log(fib(8));
+var f=0;
 const readline = require('readline').createInterface({
   input: process.stdin,
   output: process.stdout,
 });
 
-readline.question(`Enter the number`, x => {
+readline.question(`Enter the number: `, x => {
   console.log(`${fib(x-1)}`);
+
+  fs.createReadStream('data.csv')
+  .pipe(csv())
+  .on('data', (row) => {
+    // console.log("row ", row);
+    arr.push(row);
+    // console.log(arr);
+  })
+  .on('end', () => {
+    // console.log("**", arr);
+    for(let i=0; i<arr.length; i++)
+    {
+      if (arr[i]['INPUT']==x)
+      {
+        f=1;
+        break;
+      }
+    }
+    if (f==0)
+    {
+      // console.log("here")
+      arr.push({ INPUT: x, OUTPUT: fib(x-1) })
+    }
+
+    const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+    const csvWriter = createCsvWriter({
+      path: 'data.csv',
+      header: [
+        {id: 'INPUT', title: 'INPUT'},
+        {id: 'OUTPUT', title: 'OUTPUT'},
+      ]
+    });
+    csvWriter
+    .writeRecords(arr)
+    .then(()=> console.log('The CSV file was written successfully'));
+
+  });
+
   readline.close();
 });
